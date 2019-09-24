b58a596b4f1ac39062f860237086f5ae23a00ba2
BarRaider <46548278+BarRaider@users.noreply.github.com>
2019-09-23 21:39:24 -0700

    Update URLs

--- streamdeck-gpmdp/Program.cs
+++ streamdeck-gpmdp/Program.cs
@@ -11,9 +11,6 @@ namespace BarRaider.GPMDP
     {
         static void Main(string[] args)
         {
-            // Uncomment this line of code to allow for debugging
-            //while (!System.Diagnostics.Debugger.IsAttached) { System.Threading.Thread.Sleep(100); }
-
             SDWrapper.Run(args);
         }
     }
